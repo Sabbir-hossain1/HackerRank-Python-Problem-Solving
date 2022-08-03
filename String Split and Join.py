fb9476db1b64fb4a00673443454ea594b2e9e3a9
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2022-08-04 03:26:47 +0600

    String and Split problem solved

---
+++ String Split and Join.py
@@ -0,0 +1,11 @@
+# solution 1
+# given_string = input()
+# splitted = given_string.split(" ")
+# joined = "-".join(splitted)
+# print(joined)
+
+#solution 2
+# print(input().replace(" ","-"))
+
+#solution 3
+print("-".join(input().split(" ")))
--- Tuples.py
+++ Tuples.py
@@ -1,5 +1,10 @@
-n = int(input())
-integers = map(int,input().split())
-t = tuple(integers)
-hash_value = hash(t)
-print(hash_value)
+# solution 1
+
+# n = int(input())
+# integers = map(int,input().split())
+# t = tuple(integers)
+# print(hash(t))
+
+# solution 2
+n = input()
+print(hash(tuple(map(int, input().strip().split(" ")))))
