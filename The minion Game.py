6c219a89f553a0e8a663b6d1e051919064e6d5da
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2022-09-13 10:19:03 +0600

    The Minion Game problem solved(time limit exceded problem solved)

--- The minion Game.py
+++ The minion Game.py
@@ -1,16 +1,40 @@
 """
-You are asked to ensure that the first and last names
- of people begin with a capital letter in their passports.
- For example, alison heck should be capitalised correctly as Alison Heck.
-input: chris alan
-Output: Chris Alan
+Kevin and Stuart want to play the 'The Minion Game'.
 
-Note: in a word only the first character is capitalized.
- Example 12abc when capitalized remains 12abc.
+Game Rules
+
+Both players are given the same string, .
+Both players have to make substrings using the letters of the string .
+Stuart has to make words starting with consonants.
+Kevin has to make words starting with vowels.
+The game ends when both players have made all possible substrings.
+
+Scoring
+A player gets +1 point for each occurrence of the substring in the string .
+
+For Example:
+String  = BANANA
+Kevin's vowel beginning word = ANA
+Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
 
 """
+def minion_game(string):
+    sturt_count = 0
+    kevin_count = 0
+    length = len(string)
 
-names = input()
-for name in names[:].split():
-    names = names.replace(name,name.capitalize())
-print(names)
+    for i in range(len(string)):
+        if string[i] in "AEIOU":
+            kevin_count += (length-i)
+        else:
+            sturt_count += (length - i )
+    if sturt_count > kevin_count:
+        print("Stuart"+" "+str(sturt_count))
+    elif kevin_count > sturt_count:
+        print("Kevin"+" "+str(kevin_count))
+    else:
+        print("Draw")
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
