83b2378f4638423393772e2ffd9fa9b3671e65f9
Sabbir Hossain <sabbir.cse.duet@gmail.com>
2022-12-28 18:56:12 +0600

    findall with lookahed and lookbehind

---
+++ findAll_findIter.py
@@ -0,0 +1,4 @@
+import re
+y = [print(i) for i in re.findall(r"(?<=[QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm])[AEIOUaeiou]{2,}(?=[QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm])",input())]
+if not y:
+    print(-1)
