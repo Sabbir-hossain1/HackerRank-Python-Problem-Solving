d44c33f7e27827c9105a4cc4c20400edce1b1072
Sabbir Hossain <sabbir.cse.duet@gmail.com>
2023-01-02 15:05:42 +0600

    password validation regexp

---
+++ password Validation.py
@@ -0,0 +1,10 @@
+# Passwords
+# Length 8 to 15
+# At least 1 digit
+# At least 1 a-z
+# At least 1 A-Z
+# At least 1 symbol
+
+# Complete Regular Expression for Password
+
+# ^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[~!#$%^&*()_+=\-{}[\];:<>?]).{8,15}$
